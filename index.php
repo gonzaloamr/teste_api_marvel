a6b3680853f2ff3cf3e4f1ce4a798d9f368c18ff
Gonzalo Rodriguez <gonzalo.amr@gmail.com>
2021-03-13 15:31:28 -0300

    alteração-layout
    layout de exibição

--- index.php
+++ index.php
@@ -1,1 +1,40 @@
-<?php echo('TESTANDO PULL NO GIT');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/estilo.css">
+    <title>Marvel API</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>
+            <img src="images/logo-marvel-II.png" alt="some text" width=260 height=80>
+        </h1>
+                
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+            <nav class="modulos">
+                <div class="modulo vermelho-escuro">
+                    <h3>Iron Man</h3>
+                    <ul>
+                        <li><a href="exercicio.php?dir=teste&file=teste">Exercício A</a></li>
+                    </ul>
+                </div>
+                <div class="modulo vermelho-escuro">
+                    <h3>Thor</h3>
+                    <ul>
+                        <li><a href="exercicio.php?dir=teste&file=teste">Exercício A</a></li>
+                    </ul>
+                </div>
+
+
+            </nav>
+        </div>
+    </main>
+    <footer class="rodape">
+        Desenvolvido por Gonzalo Muñoz <?= date('Y'); ?>
+    </footer>
+</body>
+</html>
