f0201fa34f6fec3fe0b5fa5414b4c7ab6f2211f8
Gonzalo Rodriguez <gonzalo.amr@gmail.com>
2021-03-14 05:12:25 -0300

    Update agent-zero.php

--- agent-zero.php
+++ agent-zero.php
@@ -1,6 +1,6 @@
 <?php
 
-$content_json = file_get_contents("https://gateway.marvel.com/v1/public/characters/1009150/stories?ts=1$apikey=6846979f3da11106792b6c223296acb6&hash=9295fd3b10a4c753da3a83fd7a700595");
+$content_json = file_get_contents("https://gateway.marvel.com/v1/public/characters/1009150/stories?ts=1&apikey=6846979f3da11106792b6c223296acb6&hash=9295fd3b10a4c753da3a83fd7a700595");
 $content = json_decode($content_json);
 
 //print_r($content);
