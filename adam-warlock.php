0d451e84de0944dec82b748d4032e92889c1ec55
Gonzalo Rodriguez <gonzalo.amr@gmail.com>
2021-03-14 11:37:52 -0300

    Update adam-warlock.php
    inserindo comentários

--- adam-warlock.php
+++ adam-warlock.php
@@ -1,9 +1,9 @@
 <?php
 
-    $content_json = file_get_contents("https://gateway.marvel.com/v1/public/characters/1010354/stories?ts=1&apikey=6846979f3da11106792b6c223296acb6&hash=9295fd3b10a4c753da3a83fd7a700595");
-    $content = json_decode($content_json);
+$content_json = file_get_contents("https://gateway.marvel.com/v1/public/characters/1010354/stories?ts=1&apikey=6846979f3da11106792b6c223296acb6&hash=9295fd3b10a4c753da3a83fd7a700595");
+$content = json_decode($content_json); //esse é o decode encarregado de transformar o json em um array no php
 
-    //print_r($content); PARA DEBUGAR O ARRAY
+//print_r($content); utilizado para depuração
 
 ?>
 
@@ -13,29 +13,38 @@
     <meta charset="UTF-8">
     <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/estilo.css">
+    <link rel="stylesheet" href="assets/css/novoestilo.css"> 
     <title>Marvel API</title>
 </head>
-    <body>
+<body class="novoestilo">    
     <header class="cabecalho">
         <h1>
-            <a href="https://gonzalorodriguez.tech"><img src="images/logo-marvel-II.png" alt="some text" width=260 height=80> </a>
+        <a href="https://gonzalorodriguez.tech"><img src="images/logo-marvel-II.png" alt="some text" width=260 height=80> </a>
         </h1>                
     </header>
+    <main class="principal">
+    <nav class="navegacao">
+        <a href="https://gonzalorodriguez.tech/" class="vermelho"> Voltar</a>
+    </nav>
         <div class="conteudo">
             <nav class="modulos">
                 <div class="modulo vermelho-escuro">
                     <h3>Stories</h3>
-                    <?php    
+                    <?php //laço utilizado para buscar as informações no json converdido   
                         foreach ($content->data->results as $character){
                         echo "Story: " . $character->title . "<br>";
                         echo "<hr>";  
                         }
                     ?>
-                 </div>
+                </div>
             </nav>
-        </div> 
+        </div>
+    </div> 
     </body>
     <footer class="rodape">
         Desenvolvido por Gonzalo Muñoz 
     </footer>
 </html>
+
+
+
