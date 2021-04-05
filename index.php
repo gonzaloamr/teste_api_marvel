d921b9177127de3f0b8335a35bc45b7fbe7669ab
Gonzalo <gonzalo.munoz@kinghost.com.br>
2021-04-05 17:59:54 -0300

    testando comit de outra IDE

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ foreach ($adamwarlock->data->results as $characteradam){
     <meta charset="UTF-8">
     <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/estilo.css">
-    <title>Marvel API</title>
+    <title>Marvel API Conection</title>
 </head>
     <body>
     <header class="cabecalho">
